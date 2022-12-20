e2f6d55dd04853441f2401f179caa7f7de8a3bf6
SalimmReza <salimnuaa@gmail.com>
2022-12-21 03:56:14 +0600

    layout 3 done

--- internship-assessment-client/src/Pages/Layout3/Layout3.js
+++ internship-assessment-client/src/Pages/Layout3/Layout3.js
@@ -2,28 +2,37 @@ import React from 'react';
 
 const Layout3 = () => {
     return (
-        <div className="overflow-x-auto">
-            <table className="table w-full">
+        <div className='px-40'>
 
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Name</th>
-                        <th>DOB</th>
-                        <th>Email</th>
 
-                    </tr>
-                </thead>
-                <tbody>
+            <div className='grid grid-cols-8 grid-rows-6 gap-x-8 gap-y-8 '>
+                <div className='bg-[#DA1E5D] text-white text-[22px] font=[700px]  rounded-lg shadow-xl row-span-3
+                col-span-8 '>
+                    <h1 className='flex justify-center items-center h-[100%]'>card1</h1>
+                </div>
+                <div className='bg-[#DA1E5D] text-white text-[22px] font=[700px] rounded-lg shadow-xl  row-span-6 col-span-2' >
+                    <h1 className='flex justify-center items-center h-[100%]'>card2</h1>
+                </div>
+                <div className='bg-[#DA1E5D] text-white text-[22px] font=[700px] rounded-lg shadow-xl  row-span-6 col-span-4' >
+                    <h1 className='flex justify-center items-center h-[100%]'>card3</h1>
+                </div>
+                <div className='bg-[#DA1E5D] text-white text-[22px] font=[700px] rounded-lg shadow-xl  row-span-6 col-span-2
+                '>
+                    <h1 className='flex justify-center items-center h-[100%]'>card4</h1>
 
-                    <tr>
-                        <th>1</th>
-                        <td>Cy Ganderton</td>
-                        <td>Quality Control Specialist</td>
-                        <td>Blue</td>
-                    </tr>
-                </tbody>
-            </table>
+                </div>
+
+
+                <div className='bg-[#DA1E5D] text-white text-[22px] font=[700px] rounded-lg shadow-xl row-span-3 
+                col-span-8' >
+                    <h1 className='flex justify-center items-center h-[100%]'>card5</h1>
+                </div>
+            </div>
+
+
+
+
+
         </div>
     );
 };
